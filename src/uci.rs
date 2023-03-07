a819f256874e862d4e995b3d0dadfae4cfb92cfe
Paulo-21 <paul.cibier@gmail.com>
2023-03-07 19:45:13 +0100

    up

--- src/uci.rs
+++ src/uci.rs
@@ -6,16 +6,17 @@ pub fn uci () {
         let mut buffer = String::new();
         io::stdin().read_line(&mut buffer).unwrap();
         let mut response = String::new();
-        let command = buffer.trim();
-       
+        let mut command = buffer.trim();
+
         if command == "uci"  {
             input_uci();
         }
         else if command == "isready" {
-            println!("readyok");
+            input_ready();
         }
-        else if command == "position" {
-
+        else if command.len() >= 8 && command[..8].cmp("position") == Ordering::Equal {
+            
+            input_position(&command[ 9..]);
         }
         else if command.len() >= 2 && command[..2].cmp("go") == Ordering::Equal {
             compute();
@@ -33,6 +34,40 @@ fn input_uci() {
     println!("id author Paul Cibier\n");
     println!("uciok");
 }
+fn input_ready() {
+    println!("readyok");
+}
+fn input_position(mut commande : &str) {
+    if commande.contains("startpos") {
+        commande = &commande[9..];
+        get_bitboard_from_startpos(commande);
+    }
+    else if commande.contains("fen") {
+        commande = &commande[3..];
+        get_bitboard_from_fen(commande);
+    }
+}
 fn compute() {
     
+}
+fn get_bitboard_from_startpos(command : &str)  { // -> Game {
+    let move_tab = command.split_ascii_whitespace();
+    let init_board = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    let white_to_play = true;
+    for one_move in move_tab {
+        /*let (a,b) = convert_move();
+        if white_to_play {
+            compute_w();
+        }
+        else {
+            compute_b();
+        }
+        return final_bitboard;
+        */
+
+        println!("{one_move}");
+    }
+}   
+fn get_bitboard_from_fen(command : &str) {
+
 }
